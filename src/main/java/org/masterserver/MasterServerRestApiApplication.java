a7c3adf746817f830fa837716c1b6bab8784a8cb
José Domingo Álvarez Caba <anthrax932yt@gmail.com>
2020-04-26 21:15:37 +0200

    MASTERSERVER-000: Project created

---
+++ src/main/java/org/masterserver/MasterServerRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.masterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterServerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterServerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/masterserver/MasterServerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.masterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterServerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2b8b80663ebe7c58da987c1429b4f7528810d804
José Domingo Álvarez Caba <anthrax932yt@gmail.com>
2020-05-03 11:39:30 +0200

    MASTERSERVER-008: Resource exists exception added

---
+++ src/main/java/org/masterserver/exception/ResourceExistsException.java
@@ -0,0 +1,27 @@
+package org.masterserver.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceExistsException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceExistsException() {
+		super();
+	}
+
+	public ResourceExistsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ResourceExistsException(String message) {
+		super(message);
+	}
+
+	public ResourceExistsException(Throwable cause) {
+		super(cause);
+	}
+
+}
